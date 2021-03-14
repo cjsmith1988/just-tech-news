dc22754c574c985f60cdfc4387eca62e6fe0270a
Curtis Smith <cj1988333@hotmail.com>
2021-03-13 23:09:47 -0500

    finished feature

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,25 +1,22 @@
-async function upvoteClickHandler(event) {
+async function deleteFormHandler(event) {
     event.preventDefault();
-  
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
     
-    const response = await fetch('/api/posts/upvote', {
-        method: 'PUT',
-        body: JSON.stringify({
-          post_id: id
-        }),
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        
         headers: {
           'Content-Type': 'application/json'
         }
     });
       
     if (response.ok) {
-        document.location.reload();
+        document.location.replace('/dashboard/');
     } else {
         alert(response.statusText);
     }
-}
+  }
   
-document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
